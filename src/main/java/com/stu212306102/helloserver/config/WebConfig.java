b140a62a8d95f0c91c1e16f016c48454e008e815
Kev1nvip <207700822+Kev1nvip@users.noreply.github.com>
2026-05-02 16:33:45 +0800

    feat:spring security接管旧鉴权

--- src/main/java/com/stu212306102/helloserver/config/WebConfig.java
+++ src/main/java/com/stu212306102/helloserver/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.stu212306102.helloserver.config;
 
-import com.stu212306102.helloserver.interceptor.AuthInterceptor;
 import com.stu212306102.helloserver.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -20,10 +19,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/api/users/login",
                                      "/api/users"); // 只放行登录
 
-        // 拦截器2：你的权限拦截器 AuthInterceptor（所有接口）
-        registry.addInterceptor(new AuthInterceptor())
-                .addPathPatterns("/**")
-                .excludePathPatterns("/api/users/login",
-                        "/api/users");
+
     }
 }
